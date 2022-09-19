8149e608d3517dace1849546262046f5654572f6
voqse <git@voqse.com>
2022-09-20 00:15:38 +0200

    Base implementation

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,61 @@
-import { PluginInterface } from '@debut/types';
 import { logger, LoggerOptions } from '@voqse/logger';
+import { PluginInterface } from '@debut/types';
+import { cli, file } from '@debut/plugin-utils';
+import path from 'path';
+import { stringify } from 'csv-stringify';
+import { createWriteStream } from 'fs';
 
 const pluginName = 'csvReport';
 
-export interface CsvReportPluginOptions extends LoggerOptions {}
+interface CsvReportMethodsInterface {
+    addData(...data: any);
+}
 
-export interface SignalsMethodsInterface {
-    broadcast(...data: any);
+export interface CsvReportPluginOptions extends LoggerOptions {
+    csvHeaders: string[];
 }
 
 export interface CsvReportPluginAPI {
-    [pluginName]: SignalsMethodsInterface;
+    [pluginName]: CsvReportMethodsInterface;
 }
 
-export interface SignalsInterface extends PluginInterface {
+export interface CsvReportPluginInterface extends PluginInterface {
     name: string;
-    api: SignalsMethodsInterface;
+    api: CsvReportMethodsInterface;
 }
 
-export function csvReportPlugin(opts: CsvReportPluginOptions): SignalsInterface {
+export function csvReportPlugin(opts: CsvReportPluginOptions): CsvReportPluginInterface {
     const log = logger(pluginName, opts);
+    const csvStringify = stringify({ header: true, columns: opts.csvHeaders });
+
+    let writeStream;
 
     return {
         name: pluginName,
         api: {
-            broadcast(...data) {},
+            addData(...data) {
+                csvStringify.write(data);
+            },
         },
 
-        // async onInit() {
-        //     log.info('Initializing plugin...');
-        // },
+        async onInit() {
+            log.info('Initializing plugin...');
 
-        async onStart() {
-            log.info('Starting Telegram Bot...');
+            const { ticker, interval } = this.debut.opts;
+            const botData = await cli.getBotData(this.debut.getName())!;
+            const savePath = `${botData?.src}/csv-report/`;
+            const datePrefix = new Date().toISOString().slice(0, 10);
+            const saveFullPath = path.resolve(savePath, `report-${datePrefix}-${ticker}-${interval}.csv`);
+
+            file.ensureFile(saveFullPath);
+            writeStream = createWriteStream(saveFullPath);
         },
 
-        // async onDispose() {
-        //     log.info('Shutting down plugin...');
-        // },
+        async onDispose() {
+            log.info('Shutting down plugin...');
+
+            csvStringify.pipe(writeStream);
+            csvStringify.end();
+        },
     };
 }
